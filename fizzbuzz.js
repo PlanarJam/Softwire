c373f747b24c0d20955da050be0e6b23e685f397
Aya <Aya.Sultan@softwire.com>
2025-07-02 09:50:36 +0100

    part 2 first rule

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,13 +1,21 @@
-for (let i = 0; i <= 100; i++) {
-    if (i % 3 == 0 && i % 5 == 0) {
-        console.log("FizzBuzz");
+for (let i = 1; i <= 100; i++) {
+    let output = ""
+    let flag = false
+    if (i % 3 == 0) {
+        output += "Fizz";
+        flag = true
+    } 
+    if (i % 5 == 0) {
+        output += "Buzz"
+        flag = true
+    } 
+    if (i % 7 == 0) {
+        output += "Bang"
+        flag = true
+    }
+    if (flag) {
+        console.log(output);
     } else {
-        if (i % 3 == 0) {
-            console.log("Fizz");
-        } else if (i % 5 == 0) {
-            console.log("Buzz");
-        } else {
-            console.log(i);
-        }
+        console.log(i);
     }
 }
