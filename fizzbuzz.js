ff82ff45efe542852525482b23e044874dc2767a
Aya <Aya.Sultan@softwire.com>
2025-07-02 12:23:58 +0100

    finished fizzbuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,20 +1,41 @@
-for (let i = 1; i <= 100; i++) {
-    let output = ""
-    let flag = false
+for (let i = 1; i <= 15015; i++) {
+    let output = "";
+    let fezz = false;
+
     if (i % 3 == 0) {
-        output += "Fizz";
-        flag = true
+        output = "Fizz";
     } 
-    if (i % 5 == 0) {
-        output += "Buzz"
-        flag = true
-    } 
-    if (i % 7 == 0) {
-        output += "Bang"
-        flag = true
+
+    if (i % 13 == 0) {
+        fezz = true;
+        output += "Fezz";
     }
-    if (flag) {
-        console.log(output);
+
+    if (i % 5 == 0) {
+        output += "Buzz";
+    } 
+
+    if (i % 7 == 0) {
+        output += "Bang";
+    }
+
+    if (i % 11 == 0) {
+        output = fezz ? "FezzBong" : "Bong";
+    }
+
+    let n = output.length;
+
+    if (n > 0) {
+        if (i % 17 == 0) {
+            reversed = ""
+            for (let i = n; i > 0; i -= 4) {
+                reversed += output.slice(i - 4, i);
+            }
+
+            console.log(reversed);
+        } else {
+            console.log(output);
+        }
     } else {
         console.log(i);
     }
